f0f427a07d00d92450f7c2497e0d710c029e828d
Lars <pinguinlars1105@gmail.com>
2025-08-05 16:09:16 +0200

    BEAM

--- source/main.cpp
+++ source/main.cpp
@@ -4,8 +4,44 @@ MicroBit uBit;
 
 int main() {
   uBit.init();
+  MicroBitImage beam[16] = {
+      MicroBitImage("0,0,0,0,0\n"
+                    "0,0,0,0,0\n"
+                    "255,0,0,0,0\n"
+                    "0,0,0,0,0\n"
+                    "0,0,0,0,0\n"),
+      MicroBitImage("0,0,0,0,0\n"
+                    "0,0,0,0,0\n"
+                    "205,255,0,0,0\n"
+                    "0,0,0,0,0\n"
+                    "0,0,0,0,0\n"),
+      MicroBitImage("0,0,0,0,0\n"
+                    "0,0,0,0,0\n"
+                    "155,205,255,0,0\n"
+                    "0,0,0,0,0\n"
+                    "0,0,0,0,0\n"),
+      MicroBitImage("0,0,0,0,0\n"
+                    "0,0,0,0,0\n"
+                    "105,155,205,255,0\n"
+                    "0,0,0,0,0\n"
+                    "0,0,0,0,0\n"),
+      MicroBitImage("0,0,0,0,0\n"
+                    "0,0,0,0,0\n"
+                    "055,105,155,205,255\n"
+                    "0,0,0,0,0\n"
+                    "0,0,0,0,0\n"),
+  };
 
-  uBit.display.scroll("UwU");
+  for (int i = 0; i < sizeof(beam); i++) {
+    if (beam[i] == nullptr) // to prevent an 030 error (or panic)
+      break;
+    uBit.display.print(beam[i]);
+    uBit.sleep(100);
+  }
 
+  /**
+   * Cleanup tasks
+   */
+  uBit.display.clear();
   release_fiber();
 }
